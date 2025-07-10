8f5faabe94c6d84e31da30b8abe405658723c0c8
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-07-10 00:16:00 -0500

    chore: deleted api config file

--- producer/api/client.go
+++ producer/api/client.go
@@ -8,6 +8,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const (
+	// how long server waits the pong before close the connection
+	PONG_WAIT = 10 * time.Second
+
+	// PINT_INTERVAL  must be lower than pong wait (in this case is 90%)
+	PINT_INTERVAL = (PONG_WAIT * 9) / 10
+)
+
 type client struct {
 	conn    *websocket.Conn
 	manager *manager
